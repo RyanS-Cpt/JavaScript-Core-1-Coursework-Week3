42d3c460fd3d5fd8f02b4454c74505e4da0c12f6
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-02-08 21:11:41 +0200

    Attempted 3-space-colonies

--- 2-mandatory/3-space-colonies.js
+++ 2-mandatory/3-space-colonies.js
@@ -4,7 +4,7 @@
   They call the planet "Alpha" and they decide that the FAMILIES whose last names start with 'A' should stay,
   while the others go on in search of other planets to call home.
 
-  Create a function that returns an array of colonisers that will stay, according to the above rules.
+  Create a function that returns an array of colonizers that will stay, according to the above rules.
 
   NOTE: don't include any element that is not a "family".
 
@@ -14,8 +14,27 @@
         Logic: Only strings that start with A, and finish with family
   
 */
+let crewPicker = function (crewArr){
+  let crewStr = crewArr.toString();
+  for (let i =0; i<crewStr.length; i++){
+    if (crewStr[i].includes("family")){
+      return crewStr[i];
+}
+  }
+}
 
-function colonisers() {}
+function colonizers(voyagersArr) {
+  let crewToStay = [];
+  let newCrew = crewPicker(voyagersArr);
+  for (let j = 0; j<newCrew.length; j++){
+    if (newCrew[j].startsWith("A")){
+      return newCrew[j];
+    }
+  }
+
+  
+      return crewToStay;
+    }  // use arr.filter()?
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -49,7 +68,7 @@ function test(test_name, actual, expected) {
   console.log(`${test_name}: ${status}`);
 }
 
-test("colonisers function works", colonisers(voyagers), [
+test("colonizers function works", colonizers(voyagers), [
   "Adam family",
   "Avery family",
   "Archer family",
